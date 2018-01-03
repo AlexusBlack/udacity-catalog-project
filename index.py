f0ef5271df6552e2adffe23fdcfc9571a7843f8f
Alex Chernov <alexusblack@gmail.com>
2018-01-03 20:41:05 +1100

    rendering categories out of array

--- index.py
+++ index.py
@@ -2,15 +2,54 @@ from flask import Flask, render_template
 
 app = Flask(__name__) 
 
-@app.route('/')
-@app.route('/index', methods = ['GET'])
+user = {
+    'authorized': True,
+    'name': 'Alex Chernov'
+}
+
+categories = (
+    {
+        'id': 0,
+        'name': 'Characters'
+    },
+    {
+        'id': 1,
+        'name': 'Planets'
+    },
+    {
+        'id': 2,
+        'name': 'Space Ships'
+    },
+    {
+        'id': 3,
+        'name': 'Races'
+    },
+    {
+        'id': 4,
+        'name': 'Factions'
+    },
+    {
+        'id': 5,
+        'name': 'Technologies'
+    }
+)
+
+@app.route('/', methods = ['GET'])
 def index_route():
-    return render_template('index.html', page = {
-        'title': 'Homepage'
-    }, user = {
-        'authorized': True,
-        'name': 'Alex Chernov'
+    return render_template('index.html', page={
+        'title': 'Homepage',
+        'has_sidebar': True
+    }, user=user, content={
+        'categories': categories
     })
+
+# @app.route('/categories', methods = ['GET'])
+# def categories_route():
+#     return render_template('index.html', page={
+#         'title': 'Categories'
+#     }, user=user, content={
+#         'categories': categories
+#     })
 
 
 if __name__ == '__main__':
